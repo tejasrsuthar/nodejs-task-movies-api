2d62e158ea0f53bce9b23b3e9560a99f400d1305
Tejas Suthar <tejact2007@gmail.com>
2022-04-03 12:21:00 +0530

    add test cases for movies resource

---
+++ src/__tests__/movies/get.movies.test.js
@@ -0,0 +1,6 @@
+const { getMovies } = require('../../services/movies');
+
+test('service getMoviesByUserId should get movies for the current user', () => {
+    expect(true).toBe(true);
+});
+
---
+++ src/__tests__/movies/post.movies.test.js
@@ -0,0 +1,14 @@
+const { addMovie } = require('../../services/movies');
+
+test('service addMovie should add movies for the premium user', () => {
+    expect(true).toBe(true);
+});
+
+test('should restrict basic user to limited allowed movies', () => {
+    expect(true).toBe(true);
+});
+
+test('service addMovie should add movies for the premium user', () => {
+    expect(true).toBe(true);
+});
+
