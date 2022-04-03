7eb3767d03efe6909f01b0d43583cbfaf8667e06
Tejas Suthar <tejact2007@gmail.com>
2022-04-03 09:42:32 +0530

    add: graceful shutdown of the service

--- src/app.js
+++ src/app.js
@@ -3,17 +3,16 @@ const logger = require('morgan');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
 const authGate = require('./middlewares/authGate');
+const gracefulShutdown = require('http-graceful-shutdown');
+
 const {
   ReasonPhrases,
   StatusCodes,
 } = require('http-status-codes');
+const { SERV_ERR, GEN_MSG } = require('./utils/constants');
+require('dotenv').config();
 
-// load env variables
-require('dotenv').config()
-
-let server = null;
-
-async function init() {
+(async () => {
   const app = express();
 
   // parse requests of content-type - application/json
@@ -21,40 +20,57 @@ async function init() {
 
   const db = require('./models');
 
-  console.log("URL", db.url);
-  
-  db.mongoose
-    .connect(db.url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-    .then(() => {
-      console.info("Connected to the database!");
-
-      // healthcheck endpoint
-      app.get('/healthcheck', (req, res) => res.status(StatusCodes.OK).send({ status: ReasonPhrases.OK }));
-
-      // logger setup
-      app.use(logger('dev'));
-
-      // request authorization gate
-      app.use(authGate);
-
-      // initialize all routes
-      routes.init(app);
-
-      // port configuration
-      const port = process.env.APP_PORT || 4000;
-
-      // listen to the specified port
-      server = app.listen(port, () => {
-        console.info(`> App listening on http://localhost:${port}`);
-      });
-    })
-    .catch(err => {
-      console.log("Cannot connect to the database!", err);
+  await db.mongoose
+    .connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true }).catch(err => {
+      console.error(SERV_ERR.CAN_NOT_CONNECT_TO_DB, err);
       process.exit();
     });
-}
 
-init();
+  console.info(GEN_MSG.DB_CONNECTED);
+
+  // healthcheck endpoint
+  app.get('/healthcheck', (req, res) => res.status(StatusCodes.OK).send({ status: ReasonPhrases.OK }));
+
+  // logger setup
+  app.use(logger('dev'));
+
+  // request authorization gate
+  app.use(authGate);
+
+  // initialize all routes
+  routes.init(app);
+
+  // port configuration
+  const port = process.env.APP_PORT || 4000;
+
+  // listen to the specified port
+  server = app.listen(port, () => {
+    console.info(GEN_MSG.APP_LISTENING_ON, `http://localhost:${port}`);
+  });
+
+  /**
+   * Gracefully shutting down the server
+   * we need to make sure we close the db connection
+   * or any other external conections/resources
+   */
+  const opts = {
+    signals: 'SIGINT SIGTERM',
+    timeout: 10000,                   // timeout: 10 secs
+    forceExit: true,                  // triggers process.exit() at the end of shutdown process
+    onShutdown: cleanup,              // shutdown function (async) - e.g. for cleanup DB, ...
+    finally: finalShutdown            // finally function (sync) - e.g. for logging
+  };
+  gracefulShutdown(server, opts);
+
+  const cleanup = async () => {
+    // close the database connection 
+    console.info(GEN_MSG.DB_CONNECTION_CLOSING);
+    await db.mongoose.connection.close();
+    console.info(GEN_MSG.DB_CONNECTION_CLOSED);
+  };
+
+  const finalShutdown = () => {
+    console.info(GEN_MSG.SERVER_SHUTTING_DOWN);
+  }
+})();
+
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,7 +7,19 @@ const AUTH_ERR = {
 
 const GEN_ERR = {
 	SOMETHING_WENT_WRONG: 'Something went wrong! Please try again later',
-	BASIC_USER_MAX_LIMIT_REACHED: 'You have reached your max limit for adding movies!'
+	BASIC_USER_MAX_LIMIT_REACHED: 'You have reached your max limit for adding movies!',
+}
+
+const GEN_MSG = {
+	APP_LISTENING_ON: 'App listening on',
+	DB_CONNECTED: 'Connected to the database',
+	DB_CONNECTION_CLOSING: 'Closing the DB connction',
+	DB_CONNECTION_CLOSED: 'DB conneciton database is closed',
+	SERVER_SHUTTING_DOWN: 'Server shutting down...',
+}
+
+const SERV_ERR = {
+	CAN_NOT_CONNECT_TO_DB: 'Cannot connect to the database!',
 }
 
 const MODELS = {
@@ -18,10 +30,10 @@ const MOVIES = {
 	USER_MOVIE_ADD_SUCCESS: 'Movie has been added successfully!',
 	ADD_LIMITS: {
 		// max limit of movies basic user can add
-		BASIC_USER: 5, 
+		BASIC_USER: 5,
 
 		// for premium user, we're assuming 0 means unlimited
-		PREMIUM_USER: 0 
+		PREMIUM_USER: 0
 	}
 }
 
@@ -30,9 +42,11 @@ const API_URLS = { BASE_URL: process.env.OMDB_API_URL }
 
 module.exports = {
 	GEN_ERR,
+	GEN_MSG,
 	AUTH_ERR,
+	SERV_ERR,
 	API_URLS,
 	USER_ROLES,
 	MOVIES,
-	MODELS
+	MODELS,
 }
